0cdf7fe1460094e7136852fa9ced3970cb78a015
NAlonsoVera <nilton.vera@softmediaconsultores.com>
2024-06-12 17:16:32 -0500

    Cambios en el login, desencriptación

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -3,17 +3,36 @@ include('../administrador/config/bd.php');
 
 $email = $_POST['username'];
 $password_user = $_POST['password'];
-$sql = "SELECT * FROM usuario WHERE cCorreo = '".$email."' AND cPassword = '".$password_user."'";
+$sql = "SELECT * FROM usuario WHERE cCorreo = '".$email."'";
 $conn = conectar();
 $result = mysqli_query($conn, $sql);
+
 if ($result->num_rows > 0) {
-    session_start();
-    $_SESSION['session_email'] = $email; 
-    echo 'success';  // Asegúrate de enviar esta respuesta
+    $row = mysqli_fetch_assoc($result);
+
+    // Verificar si el usuario es administrador (fkRol == 1)
+    if ($row['fkRol'] == 1) {
+        // Comparar directamente sin desencriptar para el administrador
+        if ($password_user === $row['cPassword']) {
+            session_start();
+            $_SESSION['session_email'] = $email; 
+            echo 'success';  // Respuesta para éxito de login
+        } else {
+            echo 'error';  // Respuesta para error de login
+        }
+    } else {
+        // Desencriptar y comparar para usuarios que no son administradores
+        if (password_verify($password_user, $row['cPassword'])) {
+            session_start();
+            $_SESSION['session_email'] = $email;
+            echo 'success';
+        } else {
+            echo 'error';
+        }
+    }
 } else {
-    echo 'error';  // Enviar una respuesta de error si las credenciales son incorrectas
+    echo 'error';  // Usuario no encontrado
 }
 
 desconectar($conn);
-
 ?>
